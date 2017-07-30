5129a79bb92c3bc4eaeb51eb2db4b07566401f4e
nathanMiniovich <nminiovich@gmail.com>
2017-07-30 02:32:27 -0400

    word_couter.cpp completed

---
+++ C02/word_counter.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main(){
+	ifstream f("word_counter.cpp");
+	int wordcount = 0;
+	string word;
+
+	while (f >> word){
+		++wordcount;
+	}
+
+	cout << "There are " << wordcount << " whitespace separated words in word_counter.cpp!" << endl;
+}
