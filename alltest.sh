04f222681db506de02ae6294f066f7343c7656a8
nminiovich <nminiovich@bloomberg.net>
2017-08-01 03:23:59 -0400

    autotest shellscript done

--- alltest.sh
+++ alltest.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+
+function test(){
+    cd $1
+    make &> /dev/null
+    ./problem_set
+    make clean &> /dev/null
+    cd ..
+}
+
+test C02
+test C04
+test C06
+#test C07
+#test C09
+#test C11
+#test C12
+#test C14
+#test C15
